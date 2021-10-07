c65eed0a84a51a8dbb78c276de9e841fa35954ad
tamirgen <tamirgenkin@gmail.com>
2021-10-07 11:14:16 +0000

    create all functions for gamme in JS

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+
+function gameFun(_value){
+
+    let playerHandS = parseInt(document.querySelector("#playerHandS").textContent);
+    let computerHandS = parseInt(document.querySelector("#computerHandS").textContent);
+    
+    let playerHand = document.querySelector(".player-hand");
+    let computerHand = document.querySelector(".computer-hand");
+
+    playerHand.src = "./assets/images/" + _value.toString() + ".png";
+
+    let randomNumber = Math.floor(Math.random()*3)+1;
+   
+    computerHand.src = "./assets/images/" + randomNumber.toString() + ".png";
+
+    if(_value == 1){
+
+        if(randomNumber == 2){ computerHandS++;}
+        if(randomNumber == 3){ playerHandS++;}
+
+    }else if(_value == 2){
+
+        if(randomNumber == 1){ playerHandS++;}
+        if(randomNumber == 3){ computerHandS++;}
+
+    }else if(_value == 3){
+
+        if(randomNumber == 1){ computerHandS++;}
+        if(randomNumber == 2){ playerHandS++;}
+
+    }
+
+    document.querySelector("#playerHandS").innerHTML = playerHandS;
+    document.querySelector("#computerHandS").innerHTML = computerHandS;
+
+}
