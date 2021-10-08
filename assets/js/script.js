9eba130e94c71e46eed7f53098598abe45997bc4
tamirgen <tamirgenkin@gmail.com>
2021-10-08 12:38:56 +0000

    add haedline to README file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,24 +13,21 @@ function gameFun(_value){
    
     computerHand.src = "./assets/images/" + randomNumber.toString() + ".png";
 
-    const ROCK_TYPE = 1;
-    const PAPER_TYPE = 2;
-    const SCISSORS_TYPE = 3;
+    
+       if(_value == 1){
 
-    if(_value == ROCK_TYPE){
+        if(randomNumber == 2){ computerHandS++;}
+        if(randomNumber == 3){ playerHandS++;}
 
-        if(randomNumber == PAPER_TYPE){ computerHandS++;}
-        if(randomNumber == SCISSORS_TYPE){ playerHandS++;}
+    }else if(_value == 2){
 
-    }else if(_value == PAPER_TYPE){
+        if(randomNumber == 1){ playerHandS++;}
+        if(randomNumber == 3){ computerHandS++;}
 
-        if(randomNumber == ROCK_TYPE){ playerHandS++;}
-        if(randomNumber == SCISSORS_TYPE){ computerHandS++;}
+    }else if(_value == 3){
 
-    }else if(_value == SCISSORS_TYPE){
-
-        if(randomNumber == ROCK_TYPE){ computerHandS++;}
-        if(randomNumber == PAPER_TYPE){ playerHandS++;}
+        if(randomNumber == 1){ computerHandS++;}
+        if(randomNumber == 2){ playerHandS++;}
 
     }
 
